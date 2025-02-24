deb25fadade9f854b06c2ec03ce76a08370a67a7
rinapermata2610 <rinapermata2610@gmail.com>
2025-02-24 10:40:29 +0700

    testrina1

--- main.c
+++ main.c
@@ -1,18 +1,7 @@
-#include <graphics.h>
-#include <conio.h>
+#include "mainmenu.h"
+#include "graphics.h"
 
 int main() {
-    int gd = DETECT, gm;
-
-    // Inisialisasi mode grafik
-    initgraph(&gd, &gm, "C:\\MinGW\\lib");
-
-    // Menggambar lingkaran di tengah layar
-    circle(320, 240, 100);
-
-    // Menunggu input sebelum keluar
-    getch();
-    closegraph();
-
+    showMainMenu();
     return 0;
 }
